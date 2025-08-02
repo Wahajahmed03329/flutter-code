e3fb5cede6d47d53a21640189f3bf5996912dd8f
wahaj ahmed khan <wahajahmedkhan065@gmail.com>
2025-08-02 01:33:36 -0700

    first commit

---
+++ Main.dart
@@ -0,0 +1,53 @@
+// void main() {
+//   //  question1
+// List<String> names = ['Ali', 'Sara', 'Ahmed'];
+// print(names);
+// // question2
+// List dayes =[];
+// dayes.add("monday,tuesday,wednesday,thersday,friday,saterday,sunday");
+// print(dayes);
+// // Question3
+// List Days =["monday","tuesday","wednesday","thersday","friday","saterday","sunday"];
+//  Days.removeLast();
+//  print(Days);
+//   Days.removeLast();
+//   print(Days);
+//    Days.removeLast();
+//    print(Days);
+//     Days.removeLast();
+//     print(Days);
+//      Days.removeLast();
+//      print(Days);
+//       Days.removeLast();
+//       print(Days);
+//        Days.removeLast();
+//         print(Days);
+// // question4
+// List number =[91,21,85,46,75];
+// number.sort();
+// number.reversed;
+// print(number);
+// messing
+// question8
+
+// List<Map<String, bool>> usersEligibility = [
+// 'name' : 'eligible': true,
+// 'name': 'Alice', 'eligible': false,
+// 'name': 'Mike', 'eligible': true,
+// "name":"Sarah", 'eligible': true,
+// 'name':'Tom', 'eligible': false,
+// ];
+// question9
+  // List<int> numbers = [12, 45, 67, 23, 89, 34];
+
+  // int max = numbers[0]; // assume first element is max
+  // for (int num in numbers) {
+  //   if (num > max) {
+  //     max = num;
+      
+  // }
+  // }
+  // print("Maximum value: $max");
+}
+
+  
