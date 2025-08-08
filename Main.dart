67cbdc5cc3936322be2393e018b034810a3df7f6
wahaj ahmed khan <wahajahmedkhan065@gmail.com>
2025-08-08 09:48:57 -0700

    first commit

--- Main.dart
+++ Main.dart
@@ -1,83 +1,110 @@
+
+
 import 'dart:io';
+import 'dart:math';
 
 void main() {
-//   //  question1
-// List<String> names = ['Ali', 'Sara', 'Ahmed'];
-// print(names);
-// // question2
-// List dayes =[];
-// dayes.add("monday,tuesday,wednesday,thersday,friday,saterday,sunday");
-// print(dayes);
-// // Question3
-// List Days =["monday","tuesday","wednesday","thersday","friday","saterday","sunday"];
-//  Days.removeLast();
-//  print(Days);
-//   Days.removeLast();
-//   print(Days);
-//    Days.removeLast();
-//    print(Days);
-//     Days.removeLast();
-//     print(Days);
-//      Days.removeLast();
-//      print(Days);
-//       Days.removeLast();
-//       print(Days);
-//        Days.removeLast();
-//         print(Days);
-// // question4
-// List number =[91,21,85,46,75];
-// number.sort();
-// number.reversed;
-// print(number);
-// messing
-// question8
-
-// List<Map<String, bool>> usersEligibility = [
-// 'name' : 'eligible': true,
-// 'name': 'Alice', 'eligible': false,
-// 'name': 'Mike', 'eligible': true,
-// "name":"Sarah", 'eligible': true,
-// 'name':'Tom', 'eligible': false,
-// ];
-// question9
-  // List<int> numbers = [12, 45, 67, 23, 89, 34];
-
-  // int max = numbers[0]; // assume first element is max
-  // for (int num in numbers) {
-  //   if (num > max) {
-  //     max = num;
-      
-  // }
-  // }
-  // print("Maximum value: $max");
-  // int step =0;
-  // print(step++);
-  // print(step++);
-  // print (++step);
-  // print (step);
-  // int step2=3;
-  // print(step2--);
-  // print (step2--);
-  // print(--step2);
-  // print(step2);
- 
- 
-//  for (int i=2;i<21; i++ +i++){
-//   print ("2 tabal $i");
-//  }
-// List num =[30,45,85];
-//  for (int i=0;i<1;i++){
-// print("exame number$num");
-//  }
-//  int login =0;
-//  while(login<=3){
-//   print ("login attempted time =$login");
-//   login++;
-//   if (login==5){
-//     break;
-//   }
-// // }
-
-// }
-
+// question1
+int std =10;
+std--;
+std--;
+print (std);
+std--;
+std--;
+std--;
+print(std);
+std--;
+std--;
+print (std);
+std--;
+print(std);
+std--;
+print(std);
+print(std);
+std--;
+print(std);
+// question2
+List stdnum=[3,9,1,6,4,2,8,5,7];
+int largestnum=stdnum[0];
+for(int i=0; i<stdnum.length;i++)
+  if(stdnum[i]>largestnum){
+    largestnum=stdnum[i];
+  }
+  print("largest number is $largestnum");
+// question3
+  for (var i = 0; i <= 10; i++) {
+    // print("running loop $i");
+    print("5 * $i = ${i * 5}");
+  }
+// question4
+print("Enter your String");
+String ? name=stdin.readLineSync()!;
+String stname=name.split('').reversed.join('');
+if(name==stname){
+  print("$name is a palindrom");
+}else{
+    print("$stname is not a palindrom"); 
+} 
+// question5
+for(int i=1;i<=4; i++){
+   for(int j=1; j<=i;j++){
+    stdout.write("$i");
+   }
+}
+// question6
   
+  List<int> ww = [2, 5, 8, 1, 10, 3, 6];
+
+  print("Numbers greater than 5 are:");
+
+  // Loop through the list
+  for (int pp in ww) {
+    if (pp > 5) {
+      print(pp);
+    } else {
+      // Just for demonstration
+      print("$pp is not greater than 5");
+    }
+  }
+
+
+//    print("");
+// question7
+String stdname="Rayyan";
+int count=0;
+stdname=stdname.toLowerCase();
+for (int i=0;i<stdname.length;i++){
+if(stdname[i]=='a'|| stdname[i]=='e'|| stdname[i]=='i' ||stdname[i]=='o'|| stdname[i]=='u'){
+  count++;
+}
+}
+//  question9
+int largest=8;
+  String characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#\$%^&*()';
+  String password = 'Afgisnoob@123';
+  
+  Random random = Random();
+  
+  while (password.length < largest) {
+    int index = random.nextInt(characters.length);
+    password += characters[index];
+  }
+  
+  print('Generated password: $password');
+  // question8
+  int number=7357428;
+int coun=0;
+int num=number.abs();
+
+while(num>0){
+num=num ~/ 10;
+coun++;
+}
+print("$number is number and count are :$coun ");
+
+// question10
+for (var i = 0; i <= 10; i++) {
+    // print("running loop $i");
+    print("8 * $i = ${i * 8}");
+  }
+}
